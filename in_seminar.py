2fa816e3599334b4c8b4031b6a68c03e51545fcf
Denis <chekmarevdenchik5@gmail.com>
2022-07-12 00:26:14 +0700

    great resolve

--- in_seminar.py
+++ in_seminar.py
@@ -1,23 +1,29 @@
-org_str = 'Hello world world wor    w o r '
+org_str = 'Hello world world worwr'
 find_str = 'worl'
+
+
+# def count_substring(text: str, substring: str) -> int:
+#     amount = 0
+#     for index in range(len(text)):
+#         if text[index] == substring[0]:
+#             for j in range(len(substring)):
+#                 try:
+#                     if substring[j] != text[index+j]:
+#                         flag = False
+#                     else:
+#                         flag = True
+#                 except IndexError:
+#                     flag = False
+#             if flag:
+#                 amount += 1
+#     return amount
 
 
 def count_substring(text: str, substring: str) -> int:
     amount = 0
-    for index in range(len(text)):
-        if text[index] == substring[0]:
-            for j in range(len(substring)):
-                try:
-                    if substring[j] != text[index+j]:
-                        flag = False
-                    else:
-                        flag = True
-                except IndexError:
-                    flag = False
-            if flag:
-                amount += 1
+    for i in range(len(text)):
+        if substring in text[i:i+len(substring)]: amount += 1
     return amount
 
 
 print(count_substring(org_str, find_str))
-
