11ef0fe092fd49b6a34bf8fbc8d36c2392350a69
Denis <chekmarevdenchik5@gmail.com>
2022-07-12 12:49:14 +0700

    004.py is done

--- 003.py
+++ 003.py
@@ -1,8 +1,7 @@
 # Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
-
 # Пример:
+# Для n = 6: {1: 2.0, 2: 2.25, 3: 2.37037037037037, 4: 2.44140625, 5: 2.4883199999999994, 6: 2.5216263717421135}
 
-# - Для n = 6: [4, 7, 10, 13, 16, 19}
 
 def getNumber(text: str) -> int:
     while True:
@@ -14,7 +13,7 @@ def getNumber(text: str) -> int:
 
 def get_list(num: int) -> int:
     list = []
-    for i in range(1, num):
+    for i in range(1, num+1):
         list.append((1 + 1/i)**i)
     return list
 
---
+++ 004.py
@@ -0,0 +1,55 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях.
+# Позиции хранятся в файле file.txt в одной строке одно число.
+
+from operator import mul
+from random import randint
+
+
+def getNumber(text: str) -> int:
+    while True:
+        numb = input(text)
+        if numb.isdigit():
+            numb = int(numb)
+            return numb
+
+
+def get_random_list(size: int, min=-100, max=101) -> list:
+    list_number = []
+    for i in range(size):
+        list_number.append(randint(min, max))
+    return list_number
+
+
+def get_mult(numbers: list) -> int:
+    indexes = input(
+        'Which positions you want to read (input through space) -> ')
+    indexes = indexes.split(' ')
+    multiplication = 1
+    for i in indexes:
+        try:
+            multiplication *= numbers[int(i)]
+        except ValueError:
+            print(f'"{i}" is not integer')
+        except IndexError:
+            print(f'"{i}" goes out of range')
+    return multiplication
+
+
+def get_mult_from_file(numbers: list, filename='file.txt') -> int:
+    with open(filename) as file:
+        multiplication = 1
+        for line in file:
+            try:
+                multiplication *= numbers[int(line)]
+            except ValueError:
+                print(f'"{line}" is not integer in file')
+            except IndexError:
+                print(f'"{line}" goes out of range')
+    return multiplication
+
+
+size = getNumber('Input the size of list -> ')
+list_numbers = get_random_list(size, -size, size+1)
+print(list_numbers)
+print(f'Result = {get_mult_from_file(list_numbers)}')
